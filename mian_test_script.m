b5ae970efefb89c8adae8e274410b4ce2bc1fd4f
Riccardo <rbaroni@arces.unibo.it>
2012-06-21 14:55:00 +0200

    - Tolto il 200
    - Aggiunto ???

--- mian_test_script.m
+++ mian_test_script.m
@@ -3,9 +3,11 @@
 
 % Create test vector
 N = 400 			% Vector Length
-tVec = 200*rand(N,1)-100;
+tVec = rand(N,1)-100;
 
 
 sVec = sort(tvec);
 
 wVec = swap(tvec);
+
+cavolissmo---------------------------------
